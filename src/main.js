2e4a26c0dc377c54774e42ae3ea8eca6e3e2084d
Segun Samuel <68255937+Shegun-istaken@users.noreply.github.com>
2023-03-09 10:51:21 +0100

    removed unused npm package

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import vClickOutside from 'v-click-outside'
-
 
 import App from './App.vue'
 import router from './router'
 
-import "@/assets/index.css"
+import '@/assets/index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(vClickOutside)
 
 app.mount('#app')
